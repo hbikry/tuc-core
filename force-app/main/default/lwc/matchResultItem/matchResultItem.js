5d924ae54d61d6fba7d984e31c3923b6bcb9e239
Hicham Bikry <hbikry@salesforce.com>
2024-11-30 13:16:00 +0100

    fix knockout stage overview display

--- force-app/main/default/lwc/matchResultItem/matchResultItem.js
+++ force-app/main/default/lwc/matchResultItem/matchResultItem.js
@@ -4,11 +4,15 @@ import TEAM_FLAGS from "@salesforce/resourceUrl/TeamFlags";
 export default class KoMatchTeamResult extends LightningElement {
   @api teamName;
   @api teamScore;
+  @api teamCode;
+  @api teamFlagCode;
   @api isWinner;
 
   get flagUrl() {
-    //return TEAM_FLAGS + '/flags/' + this.teamCode + '.png';
+    return TEAM_FLAGS + "/flags/" + this.teamFlagCode + ".png";
+  }
 
-    return TEAM_FLAGS + "/flags/MAR.png";
+  get textClass() {
+    return this.isWinner ? "slds-text-title_bold" : "slds-text-title_regular";
   }
 }
